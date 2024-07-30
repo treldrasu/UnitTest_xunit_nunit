fb299ad1092af3e4a11e5edc5d0e46ca80a6313c
treldrasu <kai@sobek-innovations.de>
2024-07-30 21:46:39 +0200

    fehler usw

--- NUnit_Test/Math/MathOperationsTests.cs
+++ NUnit_Test/Math/MathOperationsTests.cs
@@ -11,37 +11,9 @@ public class MathOperationsTests
         _mathOperations = new MathOperations();
     }
 
-    [Test]
     public void Add_WhenCalled_ReturnsSum()
     {
         int result = _mathOperations.Add(2, 3);
         Assert.That(result, Is.EqualTo(5));
-    }
-
-    [Test]
-    public void Subtract_WhenCalled_ReturnsDifference()
-    {
-        int result = _mathOperations.Subtract(5, 3);
-        Assert.That(result, Is.EqualTo(2));
-    }
-
-    [Test]
-    public void Multiply_WhenCalled_ReturnsProduct()
-    {
-        int result = _mathOperations.Multiply(4, 3);
-        Assert.That(result, Is.EqualTo(12));
-    }
-
-    [Test]
-    public void Divide_WhenCalledWithNonZeroDivider_ReturnsQuotient()
-    {
-        double result = _mathOperations.Divide(10, 2);
-        Assert.That(result, Is.EqualTo(5));
-    }
-
-    [Test]
-    public void Divide_WhenCalledWithZeroDivider_ThrowsArgumentException()
-    {
-        Assert.Throws<ArgumentException>(() => _mathOperations.Divide(10, 0));
     }
 }
