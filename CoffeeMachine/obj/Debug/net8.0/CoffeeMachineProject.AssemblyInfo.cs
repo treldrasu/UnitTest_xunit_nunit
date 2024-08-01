68291f879c8df1de98c16b5d28f5d67f04669f84
treldrasu <kai@sobek-innovations.de>
2024-08-01 13:31:00 +0200

    *

--- CoffeeMachine/obj/Debug/net8.0/CoffeeMachineProject.AssemblyInfo.cs
+++ CoffeeMachine/obj/Debug/net8.0/CoffeeMachineProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeMachineProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d68efd8acc9e4ffb2684b422a6591bd8b519ed13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a5a06dd7774a521bab4a81a794beb34147bee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeMachineProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeMachineProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
